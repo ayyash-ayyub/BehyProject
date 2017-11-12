6bc1729a6a4228f93d092ab0632db08ea2d357d4
RIfai19 <fai19011995@gmail.com>
2017-11-12 20:03:06 +0700

    updete infosehat

--- app/src/main/java/adompo/ayyash/behay/InboxFragment.java
+++ app/src/main/java/adompo/ayyash/behay/InboxFragment.java
@@ -1,18 +1,29 @@
 package adompo.ayyash.behay;
 
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import adompo.ayyash.behay.AktifitasFisikUser.AktifitasFisikUserAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class InboxFragment extends Fragment {
-
+    FloatingActionButton fbAdd;
+    private ProgressDialog progressDialog;
+    private RecyclerView rv_item;
+    private AktifitasFisikUserAdapter mAdapter;
+    String ambilEmail;
 
     public static InboxFragment newInstance() {
         InboxFragment fragment = new InboxFragment();
@@ -21,13 +32,43 @@ public class InboxFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        PrefManager prefManager = new PrefManager(getActivity());
+        ambilEmail = prefManager.getActiveEmail();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_inbox, container, false);
+        progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("Silahkan Tunggu...");
 
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        mAdapter = new AktifitasFisikUserAdapter(getActivity());
+        rv_item = (RecyclerView) rootView.findViewById(R.id.rvInbox);
+        rv_item.setLayoutManager(llm);
+        rv_item.setAdapter(mAdapter);
+
+        fbAdd = (FloatingActionButton)rootView.findViewById(R.id.fb_add);
+        fbAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(),NewMessage.class);
+                startActivity(i);
+
+                //   getActivity().startActivity(i);
+                // getActivity().startActivity(i);
+                //   getActivity().recreate();
+                //   getActivity().getLastCustomNonConfigurationInstance();
+
+
+
+
+            }
+        });
         // Inflate the layout for this fragment
 
         return rootView;
--- app/src/main/java/adompo/ayyash/behay/NewMessage.java
+++ app/src/main/java/adompo/ayyash/behay/NewMessage.java
@@ -2,6 +2,7 @@ package adompo.ayyash.behay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 
 public class NewMessage extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class NewMessage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_message);
+
+        getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
     }
 }
